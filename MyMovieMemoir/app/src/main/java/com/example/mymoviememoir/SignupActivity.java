57995067f93dfa59b6443e6b149d1cacd51d6832
Farhad-Rezwan <frez0003@student.monash.edu>
2020-05-21 05:03:28 +1000

    done with views of sign up page

--- MyMovieMemoir/app/src/main/java/com/example/mymoviememoir/SignupActivity.java
+++ MyMovieMemoir/app/src/main/java/com/example/mymoviememoir/SignupActivity.java
@@ -2,17 +2,65 @@ package com.example.mymoviememoir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignupActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+
+public class SignupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private static final String TAG = "SignupActivity";
+
+    private TextView dobDisplayDate;
+    private DatePickerDialog.OnDateSetListener dobDataSetListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        dobDisplayDate = (TextView) findViewById(R.id.tvDate);
+        dobDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        SignupActivity.this,
+                        android.R.style.Theme_Material_Light_Voice,
+                        dobDataSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+                dialog.show();
+
+
+            }
+        });
+
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.states, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         Button button = findViewById(R.id.buttonBackSignIn);
 
@@ -25,7 +73,27 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+    dobDataSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            month  = month + 1;
+            Log.d(TAG, "onDateSet: " + "/" + year + "/" + month + "/" + dayOfMonth);
+            String date = month + "/" + dayOfMonth + "/" + year;
+            dobDisplayDate.setText(date);
+        }
+    };
 
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
